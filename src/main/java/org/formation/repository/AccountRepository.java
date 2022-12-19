93c7bdc31348af067c5497781addf9ac17fc1300
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2022-12-19 10:56:00 +0100

    improve code

--- src/main/java/org/formation/repository/AccountRepository.java
+++ src/main/java/org/formation/repository/AccountRepository.java
@@ -3,5 +3,16 @@ package org.formation.repository;
 import org.formation.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, String> {
+
+    @Override
+    Optional<Account> findById(String s);
+
+    @Override
+    boolean existsById(String s);
+
+    @Override
+    void deleteById(String s);
 }
