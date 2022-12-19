fb1d939392f310119e8129285d8d71b188a411d7
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2022-12-19 10:23:46 +0100

    improve code

--- src/main/java/org/formation/entity/Account.java
+++ src/main/java/org/formation/entity/Account.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import jakarta.persistence.*;
 
 @Entity
-public class Account  {
+public abstract class Account  {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
