2c8b9083086221787ddc378e90a68ec6d9c9061e
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2022-12-19 12:19:40 +0100

    improve code

--- src/main/java/org/formation/controller/RestTransactionController.java
+++ src/main/java/org/formation/controller/RestTransactionController.java
@@ -1,12 +1,28 @@
 package org.formation.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.formation.entity.Client;
+import org.formation.entity.Transaction;
+import org.formation.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/transactions")
 @CrossOrigin(origins = {"http://localhost:4200"})
 public class RestTransactionController {
+    @Autowired
+    TransactionRepository transactionRepository;
 
+    @GetMapping("from/{id}")
+    public List<Transaction> getTransactionsFrom(@PathVariable("id") Long id) {
+        return transactionRepository.findByAccount_from_Id(id);
+    }
+
+    @GetMapping("to/{id}")
+    public List<Transaction> getTransactionsTo(@PathVariable("id") Long id) {
+        return transactionRepository.findByAccount_to_Id(id);
+    }
 }
