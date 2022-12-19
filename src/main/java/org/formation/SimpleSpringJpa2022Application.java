911b2ed2caaa06196efa8192e689791e427caaa7
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2022-12-19 13:20:15 +0100

    improve code

--- src/main/java/org/formation/SimpleSpringJpa2022Application.java
+++ src/main/java/org/formation/SimpleSpringJpa2022Application.java
@@ -12,14 +12,4 @@ public class SimpleSpringJpa2022Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleSpringJpa2022Application.class, args);
 	}
-
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/clients").allowedOrigins("http://localhost:8080");
-//			}
-//		};
-//	}
 }
