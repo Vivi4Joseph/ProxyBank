b888f45e203182b1093fdd13f437fb631d0b89bc
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2022-12-19 11:33:37 +0100

    improve code

--- src/main/java/org/formation/controller/RestClientContoller.java
+++ src/main/java/org/formation/controller/RestClientContoller.java
@@ -1,6 +1,7 @@
 package org.formation.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.formation.entity.Client;
 import org.formation.repository.ClientRepository;
@@ -21,35 +22,35 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = {"http://localhost:4200"})
 public class RestClientContoller {
 
-	@Autowired
-	ClientRepository clientRepository;
+    @Autowired
+    ClientRepository clientRepository;
 
-	@GetMapping
-	public List<Client> getClients() {
-		return clientRepository.findAll();
-	}
+    @GetMapping
+    public List<Client> getClients() {
+        return clientRepository.findAll();
+    }
 
-	@PostMapping
-	public Client createClient(@RequestBody Client client) {
-		return clientRepository.save(client);
-	}
+    @PostMapping
+    public Client createClient(@RequestBody Client client) {
+        return clientRepository.save(client);
+    }
 
-	@PutMapping
+    @PutMapping
 //	@CrossOrigin(origins = "*", methods = RequestMethod.PUT, allowedHeaders = "*")
-	public Client updateClient(@RequestBody Client client) {
-		return clientRepository.save(client);
+    public Client updateClient(@RequestBody Client client) {
+        return clientRepository.save(client);
 
-	}
+    }
 
-	@DeleteMapping("/{id}")
-	public void deleteClient(@PathVariable("id") Long id) {
-		clientRepository.deleteById(id);
-	}
+    @DeleteMapping("/{id}")
+    public void deleteClient(@PathVariable("id") Long id) {
+        clientRepository.deleteById(id);
+    }
 
-	@GetMapping("/{id}")
-	public Client getClient(@PathVariable("id") Long id) {
-		return clientRepository.findById(id).get();
-
-	}
+    @GetMapping("/{id}")
+    public Client getClient(@PathVariable("id") Long id) {
+        Optional<Client> client = clientRepository.findById(id);
+        return client.orElse(null);
+    }
 
 }
