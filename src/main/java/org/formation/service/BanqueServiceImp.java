ce01539567f2fa420a0d2f8bf195bc00f690448f
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2022-12-19 11:30:14 +0100

    improve code

--- src/main/java/org/formation/service/BanqueServiceImp.java
+++ src/main/java/org/formation/service/BanqueServiceImp.java
@@ -1,7 +1,5 @@
 package org.formation.service;
 
-import org.formation.entity.Account;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
