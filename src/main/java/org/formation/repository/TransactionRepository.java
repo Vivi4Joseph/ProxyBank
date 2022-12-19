66b2091eb38678398a14b2ab3b6fdbfc7b4aaae7
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2022-12-19 14:05:05 +0100

    improve code

--- src/main/java/org/formation/repository/TransactionRepository.java
+++ src/main/java/org/formation/repository/TransactionRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByAccountFrom_Id(Long id)
+    List<Transaction> findByAccountFrom_Id(Long id);
 
     List<Transaction> findByAccountTo_Id(Long id);
 }
